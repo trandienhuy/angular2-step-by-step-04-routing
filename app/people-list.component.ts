55a56cacfa7ee7a3f2de69c81360cf9f469df1cd
Jaime González García <jaime.gg83@gmail.com>
2016-07-03 22:41:02 +0200

    angular2-03-second-component-and-data-bindings

--- app/people-list.component.ts
+++ app/people-list.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Person } from './person';
+import { PersonDetailsComponent } from './person-details.component';
 import { PeopleService } from './people.service';
 
 @Component({
   selector: 'people-list',
+  directives: [PersonDetailsComponent],
   template: `
   <!-- this is the new syntax for ng-repeat -->
-  <ul *ngFor="#person of people">
-    <li>
-        {{person.name}}
+  <ul>
+    <li *ngFor="let person of people">
+      <a href="#" (click)="selectPerson(person)"> {{person.name}}  </a>
     </li>
   </ul>
+  <person-details [person]="selectedPerson"></person-details>
   `
 })
 export class PeopleListComponent implements OnInit{
   people: Person[] = [];
-  constructor(private _peopleService : PeopleService){ }
+  selectedPerson: Person;
+
+  constructor(private peopleService : PeopleService){ }
 
   ngOnInit(){
-    this.people = this._peopleService.getAll();
+    this.people = this.peopleService.getAll();
+  }
+
+  selectPerson(person: Person){
+    this.selectedPerson = person;
   }
 
 }
